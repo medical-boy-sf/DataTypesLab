a81bf93c4b4f81ca494c642edb1efdc059172013
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-31 14:13:59 +0200

    floatOrInteger.js

--- 03.floatOrInteger.js
+++ 03.floatOrInteger.js
@@ -1,7 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function floatOrInteger(num1, num2, num3) {
+    let sum = Number(num1) + Number(num2) + Number(num3);
+    if (Math.ceil(sum) === Math.floor(sum)) {
+       console.log(`${sum} - Integer`);
+    } else {
+        console.log(`${sum} - Float`);
+    }
+}
